9976a0d0221c3cf2de668c31174f9608bc196bce
cosmic <liapandaraoan@gmail.com>
2021-06-18 16:46:19 +0800

    latest commit

--- data/class/dynamicHelp.js
+++ data/class/dynamicHelp.js
@@ -1,61 +1,116 @@
 const { Client, Message, MessageEmbed } = require("discord.js");
 
 /**
- * A function to create send a help embed message
- * @param {Client} client - Your Discord Client
+ * @param {Client} client - The Discord Client
  * @param {Message} message - The Discord Message
- * @param {string} input - The command you are looking for
- * @param {string} prefix - Your bot's prefix
- * @param {string} color - Your embed color
- * @returns {Message} Discord Message
- * @example DynamicHelp(Client, Message, "ping", "?");
+ * @param {string} input - The Bot Command
+ * @param {string} prefix - The Bot's Prefix
+ * @param {string} color - The Embed Color
  */
 
 function dynamicHelp(client, message, input, prefix, color) {
     if (!client || !(client instanceof Client))
-        throw new Error("[cosmxc] => \"client\" is a required parameter and must be a Discord Client!");
+        throw new Error("Cosmxc Error: \"client\" is a required parameter and must be a Discord Client!");
 
     if (!message || !(message instanceof Message))
-        throw new Error("[cosmxc] => \"message\" is a required parameter and must be a Discord Message!");
+        throw new Error("Cosmxc Error: \"message\" is a required parameter and must be a Discord Message!");
 
     if (!prefix || typeof prefix !== "string")
-        throw new Error("[cosmxc] => \"prefix\" is a required parameter and must be a String!");
+        throw new Error("Cosmxc Error: \"prefix\" is a required parameter and must be a String!");
 
     if (!client.commands)
-        throw new Error("[cosmxc] => Failed to find your commands using \"client.commands\"");
+        throw new Error("Cosmxc Error: Failed to find your commands using \"client.commands\"");
 
-    const cmd = client.commands.get(input.toLowerCase()) || client.commands.get(client.aliases.get(input.toLowerCase()));
+    if (!input) {
+        const { commands, owners } = client;
+        const categories = new Set();
+        const fields = [];
 
-    if (!cmd) {
-        const noInfo = new MessageEmbed()
-            .setColor("RED")
-            .setDescription(`❌ No information found for \`${input.toLowerCase()}\``)
-        return message.channel.send(noInfo)
+        commands.forEach((cmd) => {
+            if (cmd.ownerOnly && !owners.includes(message.author.id)) return;
+            const cmdCategory = cmd.category || "Misc";
+            categories.add(cmdCategory);
+        });
+
+        Array.from(categories).sort()
+        categories.forEach((cat) => {
+            const field = [];
+            commands.forEach((cmd) => {
+                if (cmd.ownerOnly && !owners.includes(message.author.id)) return;
+                if (!cmd.category && cat == "Misc")
+                    field.push(cmd);
+                if (cmd.category == cat) field.push(cmd);
+            });
+
+            let fieldName = `${nicerString(cat)} Commands [${field.length}]`
+            if (cat.toLowerCase().includes('command')) fieldName = `${nicerString(cat)} [${field.length}]`
+
+            fields.push({
+                name: fieldName,
+                value: field.map((cmd) => `\`${cmd.name}\``).join(" "),
+                inline: false,
+            });
+        });
+
+        const embed = new MessageEmbed()
+            .setAuthor(`${client.user?.username} Commands List`, client.user?.displayAvatarURL())
+            .setDescription(`Use \`${prefix}help <command>\` to get more info on a command.`)
+            .addFields(fields)
+            .setFooter(`${client.commands.size} commands`)
+            .setTimestamp()
+            .setColor(color)
+        return message.channel.send(embed);
     }
 
-    let info = `**Command Name:** \`${cmd.name || 'No command name.'}\`\n`;
+    try {
+        const cmd = client.commands.get(input.toLowerCase()) || client.commands.get(client.aliases.get(input.toLowerCase()));
 
-    if (cmd.aliases)
-        info += `**Aliases**: ${cmd.aliases.map((a) => `\`${a}\``).join(", ")}\n`;
+        if (!cmd) {
+            const noInfo = new MessageEmbed()
+                .setColor("RED")
+                .setDescription(`❌ No information found for \`${nicerString(input)}\``)
+            return message.channel.send(noInfo)
+        }
 
-    if (cmd.cooldown)
-        info += `**Cooldown**: ${cmd.cooldown}\n`;
+        let info = `**Command Name:** \`${cmd.name || 'No command name.'}\`\n`;
 
-    if (cmd.example)
-        info += `**Example**: ${cmd.example}\n`;
+        if (cmd.aliases)
+            info += `**Aliases**: ${cmd.aliases.map((a) => `\`${a}\``).join(", ")}\n`;
 
-    info +=
-        `**Description:** \`${cmd.description || 'No description.'}\`
+        if (cmd.cooldown)
+            info += `**Cooldown**: ${cmd.cooldown}\n`;
+
+        if (cmd.example)
+            info += `**Example**: ${cmd.example}\n`;
+
+        info +=
+            `**Description:** \`${cmd.description || 'No description.'}\`
         **Category**: \`${cmd.category || 'No category.'}\`
         **Usage**: \`${prefix}${cmd.usage || cmd.name}\`
         **Usage Syntax**: \`<> = required, [] = optional\``;
 
-    const helpEmbed = new MessageEmbed()
-        .setColor(color)
-        .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-        .setDescription(info)
-        .setFooter(`Commands: ${client.commands.size || 'Unknown'} | Prefix: ${prefix}`, message.author.displayAvatarURL({ dynamic: true }))
-    return message.channel.send(helpEmbed);
+        const helpEmbed = new MessageEmbed()
+            .setColor(color)
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+            .setDescription(info)
+            .setFooter(`Commands: ${client.commands.size || 'Unknown'} | Prefix: ${prefix}`, message.author.displayAvatarURL({ dynamic: true }))
+        return message.channel.send(helpEmbed);
+    } catch {
+        const noInfo = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`❌ No information found for \`${nicerString(input)}\``)
+        return message.channel.send(noInfo)
+    }
+}
+
+function nicerString(str) {
+    var splitStr = str.toLowerCase().split(" ");
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(" ");
 }
 
 module.exports = dynamicHelp;
+
+
